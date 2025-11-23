ca5074cc89a989eb88a0cc003d16307f42f9ed6a
karthick <karthickgeetha1998@gmail.com>
2025-11-23 18:45:36 +0530

    Attendance refactor

--- src/main/java/com/infosys/seatsync/service/impl/AttendanceServiceImpl.java
+++ src/main/java/com/infosys/seatsync/service/impl/AttendanceServiceImpl.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
@@ -27,27 +28,40 @@ public class AttendanceServiceImpl implements AttendanceService {
     public AttendanceResponseDto markAttendanceForEmp(String emp, String location, String status) {
 
         try {
-            AttendanceResponseDto attendanceResponseDto = new AttendanceResponseDto();
 
-            AttendanceLog attendanceLog = new AttendanceLog();
-            Optional<Employee> employeeOpt = employeeRepository.findById(emp);
-            if(employeeOpt.isEmpty()){
-                attendanceResponseDto.setStatus("INVALID_EMP");
-                attendanceResponseDto.setMessage("Unable to fetch the emp object");
+            LocalDate today = LocalDate.now();
+            LocalDateTime startOfDay = today.atStartOfDay();
+            LocalDateTime endOfDay = today.atTime(23, 59, 59);
+
+
+            //level 1 - Check if the emp has attendance record for the same date
+            boolean isAttendanceExists = attendanceLogRepository.existsByEmployee_EmpIdAndSwipeTimeBetween(emp, startOfDay, endOfDay);
+            if(!isAttendanceExists){
+                AttendanceResponseDto attendanceResponseDto = new AttendanceResponseDto();
+
+                AttendanceLog attendanceLog = new AttendanceLog();
+                Optional<Employee> employeeOpt = employeeRepository.findById(emp);
+                if(employeeOpt.isEmpty()){
+                    attendanceResponseDto.setStatus("INVALID_EMP");
+                    attendanceResponseDto.setMessage("Unable to fetch the emp object");
+                    return attendanceResponseDto;
+                }
+                attendanceLog.setEmployee(employeeOpt.get());
+                attendanceLog.setLocation(location);
+                attendanceLog.setSwipeTime(LocalDateTime.now());
+                attendanceLog.setStatus(status);
+                attendanceLogRepository.save(attendanceLog);
+
+                attendanceResponseDto.setTimestamp(Instant.now());
+                attendanceResponseDto.setStatus("SUCCESS");
+                attendanceResponseDto.setMessage("Employee Attendance has been recorded");
                 return attendanceResponseDto;
+            } else {
+                throw new BusinessException("ATTENDANCE_EXISTS", "Attendance record already is already present ");
             }
-            attendanceLog.setEmployee(employeeOpt.get());
-            attendanceLog.setLocation(location);
-            attendanceLog.setSwipeTime(LocalDateTime.now());
-            attendanceLog.setStatus(status);
-            attendanceLogRepository.save(attendanceLog);
 
-            attendanceResponseDto.setTimestamp(Instant.now());
-            attendanceResponseDto.setStatus("SUCCESS");
-            attendanceResponseDto.setMessage("Employee Attendance has been recorded");
-            return attendanceResponseDto;
         } catch (Exception exception){
-            throw new BusinessException("ERROR", "Unable to process the attendance request, Try Again!");
+            throw new BusinessException("ERROR", exception.getMessage());
         }
     }
 }
